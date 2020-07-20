7031a114b8db27369aaadfcabec5c58f4d90245c
colmclovin <thecanadianbaconstrip@gmail.com>
2020-07-19 22:39:35 -0600

    users can see reviews

--- app.js
+++ app.js
@@ -5,20 +5,28 @@ var exphbs = require('express-handlebars');
 
 
 //start app
-const app = express()
+const app = express();
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-
+let reviews = [
+    { title: "Great Review", movieTitle: "Batman II" },
+    { title: "Awesome Movie", movieTitle: "Titanic" }
+  ];
 
 //routing
 app.get('/', (req, res) => {
-    res.render('home', { msg: 'Handlebars are Cool!' });
-  })
+    res.render('reviews-index', { msg: 'Handlebars are Cool!' });
+  });
 
-
+ 
+/*
+app.get('/reviews',(req, res) => {
+    res.render('reviews-index',{reviews: reviews})
+});
+*/
 
 //open to port
 app.listen(3000, () => {
-  console.log('App listening on port 3000!')
-})
+  console.log('App listening on port 3000!');
+});
